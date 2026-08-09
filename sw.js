11f5bb8184544bb38895720f33fe911907110623
Wekkel <wekkel_e@hotmail.com>
2026-08-09 10:03:18 +0200

    Add weather system and ship damage scripts to cache
    patch 4 and 5 updates

--- sw.js
+++ sw.js
@@ -80,7 +80,9 @@ const SHELL = [
   'js/simulation/surface-watch.js',
   'js/rendering/bridge-3d.js',
   'js/simulation/sound-radar.js',
-  'js/rendering/sound-room.js'
+  'js/rendering/sound-room.js',
+  'js/simulation/weather-system.js',
+  'js/simulation/ship-damage.js'
 ];
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
