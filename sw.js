b8bfbdc7ef5e1bf5b7bce58f0385e4a19e4c8b51
Wekkel <wekkel_e@hotmail.com>
2026-08-14 09:38:17 +0200

    Introduce OPTIONAL_SHELL for additional assets
    Add optional shell assets for caching in service worker

--- sw.js
+++ sw.js
@@ -99,6 +99,14 @@ const SHELL = [
   './js/rendering/battle-atmosphere.js',
   './js/audio/audio-director.js'
 ];
+
+const OPTIONAL_SHELL = new Set([
+  './periscope-patrol-icon-192-v3.png',
+  './periscope-patrol-icon-512-v3.png',
+  './periscope-patrol-icon-maskable-512-v3.png',
+  './periscope-patrol-apple-touch-icon-v3.png'
+]);
+
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE);
